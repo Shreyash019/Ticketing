556b2171a90e9bf07cb9168b12b31aa14b9c8ff9
Shreyash Kumar <shreyashgaur1@gmail.com>
2024-02-29 23:18:10 +0530

    User Authentication

--- backend/user/controllers/userAuthController.js
+++ backend/user/controllers/userAuthController.js
@@ -1,34 +1,87 @@
 const CatchAsync = require('../errors/catchAsync');
-const {HttpStatusCode} = require('../enums/httpHeaders');
+const ErrorHandler = require('../utils/errorHandler');
+const { HttpStatusCode } = require('../enums/httpHeaders');
+const authToken = require('../utils/authToken');
+const Users = require('../models/Users');
 
 
 // Sign Up
-exports.ticketing_User_Sign_Up = CatchAsync( async(req, res, next)=>{
+exports.ticketing_User_Sign_Up = CatchAsync(async (req, res, next) => {
+
+    // Checking for All Details
+    if (!req.body.firstName || !req.body.lastName || !req.body.userName || !req.body.email || !req.body.password) {
+        return res.status(HttpStatusCode.BAD_REQUEST).json({
+            success: false,
+            message: `Please provide all details!`
+        })
+    }
+
+    // Checking if user exist
+    const isUser = await Users.findOne({ $or: [{ email: req.body.email }, { userName: req.body.userName }] })
+    if (isUser) {
+        return res.status(HttpStatusCode.CONFLICT).json({
+            success: false,
+            message: `User already exist either by user name or email!`
+        })
+    }
+
+    // Saving Details
+    const user = await Users.create({
+        firstName: req.body.firstName.toLowerCase(),
+        lastName: req.body.lastName.toLowerCase(),
+        userName: req.body.userName.trim(' ').toLowerCase(),
+        email: req.body.email.toLowerCase(),
+        password: req.body.password
+    });
+
+    // Setting Token
+    await authToken.sendToken(res, user);
+
+    // Sending Response
     res.status(HttpStatusCode.SUCCESS).json({
         success: true,
-        message: 'Registration Successful'
+        message: 'User Registration Successful!'
     })
 })
-
-// Account Verification
-exports.ticketing_User_Registration_Verification = CatchAsync( async(req, res, next)=>{
-    res.status(HttpStatusCode.SUCCESS).json({
-        success: true,
-        message: 'Verification Successful'
-    })
-})
-
-// Resend OTP
-exports.ticketing_User_Resend_Registration_OTP = CatchAsync( async(req, res, next)=>{
-    res.status(HttpStatusCode.SUCCESS).json({
-        success: true,
-        message: 'OTP Sent Successfully!'
-    })
-})
-
 
 //  Log In 
-exports.ticketing_User_Sign_In = CatchAsync( async(req, res, next)=>{
+exports.ticketing_User_Sign_In = CatchAsync(async (req, res, next) => {
+    // Destructuring request body
+    const { password } = req.body;
+    if (!password || typeof password !== "string" || (!req.body.username && !req.body.email)) {
+        return next(new ErrorHandler(`Please provide credentials!`, HttpStatusCode.NOT_ACCEPTABLE));
+    }
+    if ((!req.body.username && typeof req.body.email !== "string") || (typeof req.body.username !== "string" && !req.body.email)) {
+        return next(new ErrorHandler(`Please provide appropriate credentials!`, HttpStatusCode.NOT_ACCEPTABLE));
+    }
+
+    let user;
+    if (req.body.username) {
+        // Checking if user exist
+        user = await Users.findOne({ username: req.body.username })
+        // Checking password are same or not
+        if (!user || !(await user.correctPassword(password, user.password))) {
+            return next(new ErrorHandler("Invalid email or password", HttpStatusCode.UNAUTHORIZED));
+        }
+    } else if (req.body.email) {
+        // Checking if user exist
+        user = await Users.findOne({ email: req.body.email })
+
+        // Checking password are same or not
+        if (!user || !(await user.correctPassword(password, user.password))) {
+            return next(new ErrorHandler("Invalid email or password", HttpStatusCode.UNAUTHORIZED));
+        }
+    }
+
+    user.forgotOTP.otp = undefined;
+    user.forgotOTP.timeToExpire = undefined;
+    user.forgotOTP.OTPVerified = undefined;
+    await user.save();
+
+    // Setting cookie and sending response
+    await authToken.sendToken(res, user);
+
+    // Sending response
     res.status(HttpStatusCode.SUCCESS).json({
         success: true,
         message: 'Login Successful!'
@@ -36,15 +89,23 @@ exports.ticketing_User_Sign_In = CatchAsync( async(req, res, next)=>{
 })
 
 // Sign Out
-exports.ticketing_User_Sign_Out = CatchAsync( async(req, res, next)=>{
+exports.ticketing_User_Sign_Out = CatchAsync(async (req, res, next) => {
+    //  Setting null value for header authorization and cookie
+    res.removeHeader("Authorization");
+    res.cookie("usertoken", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true,
+    });
+
+    // Sending response
     res.status(HttpStatusCode.SUCCESS).json({
         success: true,
-        message: 'Logout Successful!'
-    })
+        message: "You are logged out.",
+    });
 })
 
 // Password Change
-exports.ticketing_User_Password_Update = CatchAsync( async(req, res, next)=>{
+exports.ticketing_User_Password_Update = CatchAsync(async (req, res, next) => {
     res.status(HttpStatusCode.SUCCESS).json({
         success: true,
         message: 'Password Updated Successfully'
@@ -52,7 +113,7 @@ exports.ticketing_User_Password_Update = CatchAsync( async(req, res, next)=>{
 })
 
 // Forgot Password
-exports.ticketing_User_Forgot_Password = CatchAsync( async(req, res, next)=>{
+exports.ticketing_User_Forgot_Password = CatchAsync(async (req, res, next) => {
     res.status(HttpStatusCode.SUCCESS).json({
         success: true,
         message: 'Reset Email Sent Successfully!'
@@ -60,7 +121,7 @@ exports.ticketing_User_Forgot_Password = CatchAsync( async(req, res, next)=>{
 })
 
 // Reset Password
-exports.ticketing_User_Reset_Password = CatchAsync( async(req, res, next)=>{
+exports.ticketing_User_Reset_Password = CatchAsync(async (req, res, next) => {
     res.status(HttpStatusCode.SUCCESS).json({
         success: true,
         message: 'Account Recovery Successfully!'
---
+++ backend/user/utils/authToken.js
@@ -0,0 +1,137 @@
+// Builtin Modules Import
+const jwt = require('jsonwebtoken');
+
+// Database Import
+const Users = require('../models/Users.js');
+const catchAsync = require('../error/catchAsync.js');
+const { HttpStatusCode } = require('../enums/httpHeaders.js');
+
+const authToken = {
+
+    // 01) <<<<<<<<|| TOKEN GENERATION ||>>>>>>>>
+    userSignToken: function (id) {
+        return jwt.sign({ id }, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXPIRES_IN
+        })
+    },
+
+    // 02) <<<<<<<<|| TOKEN SETUP FOR USER ||>>>>>>>>
+    sendToken: function (res, user) {
+
+        // a) Token Generation
+        const token = this.userSignToken(user._id);
+
+        // b) Cookie validation days setup
+        const options = {
+            expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+            httpOnly: true,
+        }
+
+        // c) Token setting in header
+        res.cookie('usertoken', token, options);
+
+        // Sending response 
+        return res
+    },
+
+    // 03) <<<<<<<<|| AUTHENTICATION CHECK ||>>>>>>>>
+    isAuthenticated: catchAsync(async function (req, res, next) {
+
+        // a) Fetching token 
+        let token = undefined;
+
+        if (req.cookies.usertoken) {
+            token = req.cookies.usertoken
+        }
+        // else if (req.headers.authorization && req.headers.authorization.startsWith('bearer')) {
+        else if (req.headers.authorization && req.headers.authorization.startsWith('bearer')) {
+            if (req.headers.authorization.split(' ')[1].toString().toLowerCase() !== 'null') {
+                token = req.headers.authorization.split(' ')[1];
+            }
+        }
+
+        // b) Returning if no token
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: `Please login.`
+            })
+        }
+
+        // c) Decoding user using token
+        function verifyToken(token) {
+            return new Promise((resolve, reject) => {
+                jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+                    if (err && err.name === 'JsonWebTokenError' && err.message === 'jwt malformed') {
+                        reject(new Error('Invalid token'));
+                    } else if (err) {
+                        reject(err);
+                    } else {
+                        resolve(decoded);
+                    }
+                });
+            });
+        }
+
+        let jwtReturnData = {
+            decoded: undefined,
+            err: undefined
+        };
+        await verifyToken(token)
+            .then(data => {
+                jwtReturnData.decoded = data
+            })
+            .catch(err => {
+                jwtReturnData.err = err.message
+            });
+
+        if (jwtReturnData.err) {
+            return res.status(HttpStatusCode.UNAUTHORIZED).json({
+                success: false,
+                message: `Bad Request! Please login again.`
+            })
+        }
+        // const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        let user = await Users.findById(jwtReturnData.decoded.id)
+            .select('+role +isBusinessProfile')
+
+        // d) Setting Authenticated User
+        if (!user) {
+            return next(new ErrorHandler(`Please login again`, 401))
+        }
+        req.user = user
+
+        // e) Calling next function
+        next();
+    }),
+
+    // 04) <<<<<<<<|| PROFILE VERIFICATION CHECK||>>>>>>>>
+    isProfileVerified: async function (req, res, next) {
+        let user = await Users.findById(req.user.id)
+            .select("+isAccountVerified +isProfileCompleted")
+
+        // d) Setting Authenticated User
+        if (!user) {
+            return next(new ErrorHandler(`Either user not exist or not logged in!`, 401))
+        } else {
+            if (!user.isAccountVerified) {
+                return next(new ErrorHandler(`Your account is not verified, Please verify it first!`, 401))
+            }
+            if (!user || !user.isProfileCompleted) {
+                return next(new ErrorHandler(`Your profile is not updated yet, Please update it first!`, 401))
+            }
+        }
+
+        next();
+    },
+
+    // 05) <<<<<<<<|| CLEARING SENSITIVE DATA ||>>>>>>>>
+    userDataClear: async function (req, res, next) {
+        req.user.role = undefined,
+        req.user.isBusinessProfile = undefined
+        next();
+    },
+
+}
+
+module.exports = authToken;
